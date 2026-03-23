14aa02e5a7b12abe6c25c57aab0422f7b9eee95f
Chaofan Lin <chaofan@deepseek.com>
2026-03-23 19:26:24 +0800

    [Feature] Support alloc global workspace (#1940)

--- tilelang/engine/phase.py
+++ tilelang/engine/phase.py
@@ -268,6 +268,7 @@ def OptimizeForTarget(mod: IRModule, target: Target) -> IRModule:
         mod = tilelang.transform.PlanAndUpdateBufferAllocationLocation()(mod)
         mod = tilelang.transform.PipelinePlanning()(mod)
         mod = tilelang.transform.InjectSoftwarePipeline()(mod)
+    mod = tilelang.transform.HoistGlobalBufferAllocations()(mod)
     mod = tilelang.transform.LowerOpaqueBlock()(mod)
     mod = tilelang.transform.Simplify()(mod)
     mod = tilelang.transform.Simplify()(mod)
