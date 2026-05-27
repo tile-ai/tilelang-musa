xwhzz/tilelang

[Feature] Support T.annotate_compile_flags, T.annotate_pass_configs, and out_idx as PrimFunc attrs (#2006)
Allow configuring pass configs, compile flags, and out_idx directly
inside function bodies using T.annotate_compile_flags(),
T.annotate_pass_configs(), and T.empty()+return. These are stored as
proper PrimFunc attrs (tilelang_compile_flags, tilelang_pass_configs,
tilelang_out_idx) instead of monkey-patching, and merged at compile
time. Annotations can be placed before or after tensor type annotations.

Co-authored-by: Claude Opus 4.6 <noreply@anthropic.com>

testing/python/language/test_tilelang_language_func_attrs.py
@@ -1,0 +1,214 @@
+"""Test T.annotate_compile_flags, T.annotate_pass_configs, and out_idx via PrimFunc attrs."""
+
+import pytest
+import torch
+import tilelang
+from tilelang import language as T
+from tilelang.transform import PassConfigKey
+
+
+def test_out_idx_via_attr_lazy():
+    """out_idx should be stored as PrimFunc attr when using T.empty + return."""
+
+    @T.prim_func
+    def kernel(A):
+        A: T.Tensor[[128, 128], T.float32]
+        B = T.empty([128, 128], T.float32)
+        with T.Kernel(1):
+            for i in T.serial(128):
+                for j in T.serial(128):
+                    B[i, j] = A[i, j] + 1.0
+        return B
+
+    assert "tilelang_out_idx" in kernel.attrs
+    assert list(kernel.attrs["tilelang_out_idx"]) == [-1]
+
+    compiled = tilelang.compile(kernel)
+    a = torch.randn(128, 128, device="cuda")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+def test_all_attrs_together_lazy():
+    """annotate_pass_configs, annotate_compile_flags, and out_idx should all work together."""
+
+    @T.prim_func
+    def kernel(A):
+        A: T.Tensor[[64, 64], T.float32]
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        T.annotate_compile_flags(["--use_fast_math"])
+        B = T.empty([64, 64], T.float32)
+        with T.Kernel(1):
+            for i in T.serial(64):
+                for j in T.serial(64):
+                    B[i, j] = A[i, j] * 2.0
+        return B
+
+    attrs = kernel.attrs
+    assert "tilelang_out_idx" in attrs
+    assert "tilelang_pass_configs" in attrs
+    assert "tilelang_compile_flags" in attrs
+
+    compiled = tilelang.compile(kernel)
+    a = torch.randn(64, 64, device="cuda")
+    b = compiled(a)
+    torch.testing.assert_close(b, a * 2.0)
+
+
+def test_eager_mode_attrs():
+    """Eager mode should support annotate_pass_configs and out_idx via T.empty."""
+
+    @tilelang.jit
+    def kernel(A):
+        M, N = T.const("M N")
+        A: T.Tensor[[M, N], T.float32]
+        B = T.empty([M, N], T.float32)
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        with T.Kernel(1):
+            for i in T.serial(M):
+                for j in T.serial(N):
+                    B[i, j] = A[i, j] + 1.0
+        return B
+
+    a = torch.randn(32, 32, device="cuda")
+    result = kernel(a)
+    torch.testing.assert_close(result, a + 1.0)
+
+
+def test_out_idx_conflict_detection():
+    """Specifying both T.empty return and external out_idx should raise ValueError."""
+
+    @T.prim_func
+    def kernel(A):
+        A: T.Tensor[[32, 32], T.float32]
+        B = T.empty([32, 32], T.float32)
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j]
+        return B
+
+    with pytest.raises(ValueError, match="Out index conflict"):
+        tilelang.compile(kernel, out_idx=[-1])
+
+
+def test_no_out_idx_when_not_using_empty():
+    """When T.empty is not used, tilelang_out_idx attr should not be present."""
+
+    @T.prim_func
+    def kernel(A, B):
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j]
+
+    assert kernel.attrs is None or "tilelang_out_idx" not in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="cuda")
+    b = compiled(a)
+    torch.testing.assert_close(b, a)
+
+
+def test_pass_configs_only_lazy():
+    """annotate_pass_configs should work without T.empty or annotate_compile_flags."""
+
+    @T.prim_func
+    def kernel(A, B):
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j] + 1.0
+
+    assert "tilelang_pass_configs" in kernel.attrs
+    assert kernel.attrs is None or "tilelang_out_idx" not in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="cuda")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+def test_compile_flags_only_lazy():
+    """annotate_compile_flags should work standalone."""
+
+    @T.prim_func
+    def kernel(A, B):
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        T.annotate_compile_flags(["--use_fast_math"])
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j] + 1.0
+
+    assert "tilelang_compile_flags" in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="cuda")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+def test_annotations_before_tensor_type():
+    """Annotations placed before tensor type annotations should work."""
+
+    @T.prim_func
+    def kernel(A, B):
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        T.annotate_compile_flags(["--use_fast_math"])
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j] + 1.0
+
+    assert "tilelang_pass_configs" in kernel.attrs
+    assert "tilelang_compile_flags" in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="cuda")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+def test_annotations_after_tensor_type():
+    """Annotations placed after tensor type annotations should work."""
+
+    @T.prim_func
+    def kernel(A, B):
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        T.annotate_compile_flags(["--use_fast_math"])
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j] + 1.0
+
+    assert "tilelang_pass_configs" in kernel.attrs
+    assert "tilelang_compile_flags" in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="cuda")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+if __name__ == "__main__":
+    test_out_idx_via_attr_lazy()
+    test_all_attrs_together_lazy()
+    test_eager_mode_attrs()
+    test_out_idx_conflict_detection()
+    test_no_out_idx_when_not_using_empty()
+    test_pass_configs_only_lazy()
+    test_compile_flags_only_lazy()
+    test_annotations_before_tensor_type()
+    test_annotations_after_tensor_type()
+    print("All tests passed!")

tilelang/language/eager/builder.py
@@ -181,6 +181,8 @@ class Builder(BaseBuilder):
         self.constexpr_var = set()
         self.eager_jit: EagerJITStage = "none"
         self.eager_jit_subs: dict[str, PrimExpr] = {}
+        self.func_pass_configs: dict[str, Any] | None = None
+        self.func_compile_flags: list[str] | str | None = None
         self.current_file = "<unknown>"
         self.current_line = 0
         self.current_macro_name = "<unknown-macro>"
@@ -754,7 +756,6 @@ if TYPE_CHECKING:
         span: Span | None
         ir_gen: IRGenerator[_P, _T] | None
         orig_func: Callable[_P, _T] | None
-        out_idx_override: list[int] | None
 
 else:
     PrimFunc = tvm.tir.PrimFunc
@@ -935,6 +936,67 @@ def const(name: str, dtype: str = "int32") -> Var | tuple[Var, ...]:
             return builder.eager_jit_subs[name]
 
 
+def annotate_compile_flags(flags: list[str] | str) -> None:
+    """
+    Annotate additional device compile flags inside a function body.
+
+    The flags will be merged with any externally provided compile_flags
+    at compilation time. Can be placed before or after tensor type annotations.
+
+    Example::
+
+        @tilelang.jit
+        def kernel(A, B):
+            T.annotate_compile_flags(["--use_fast_math"])
+            ...
+    """
+    builder = Builder.current()
+    if builder is None:
+        raise JITNoBuilderError("T.annotate_compile_flags() can only be used inside @tilelang.jit or @T.prim_func")
+    if builder.eager_jit == "phase1":
+        return
+    builder.func_compile_flags = flags
+
+
+def annotate_pass_configs(configs: dict[str, Any]) -> None:
+    """
+    Annotate pass configuration inside a function body.
+
+    The configs will be merged with any externally provided pass_configs
+    at compilation time (function-level configs take lower priority, i.e.
+    external configs override). Can be placed before or after tensor type annotations.
+
+    Example::
+
+        @tilelang.jit
+        def kernel(A, B):
+            T.annotate_pass_configs({
+                PassConfigKey.TL_ENABLE_FAST_MATH: True,
+            })
+            ...
+    """
+    builder = Builder.current()
+    if builder is None:
+        raise JITNoBuilderError("T.annotate_pass_configs() can only be used inside @tilelang.jit or @T.prim_func")
+    if builder.eager_jit == "phase1":
+        return
+    builder.func_pass_configs = configs
+
+
+def _patch_prim_func_attrs(pf: PrimFunc, builder: Builder) -> PrimFunc:
+    """Attach function-level out_idx, pass_configs and compile_flags as PrimFunc attrs."""
+    if builder.out_idx:
+        pf = pf.with_attr("tilelang_out_idx", builder.out_idx)
+    if builder.func_pass_configs is not None:
+        pf = pf.with_attr("tilelang_pass_configs", builder.func_pass_configs)
+    if builder.func_compile_flags is not None:
+        flags = builder.func_compile_flags
+        if isinstance(flags, str):
+            flags = [flags]
+        pf = pf.with_attr("tilelang_compile_flags", flags)
+    return pf
+
+
 @dataclass
 class TirTemplate(Generic[_P, _T]):
     """
@@ -1019,8 +1081,7 @@ class TirTemplate(Generic[_P, _T]):
         with builder.prim_func(self.name):
             self.ir_gen.gen(builder)(**tensor_args, **kwargs)
         pf = builder.get()
-        if builder.out_idx:
-            pf.out_idx_override = builder.out_idx
+        pf = _patch_prim_func_attrs(pf, builder)
         return pf
 
 
@@ -1117,8 +1178,6 @@ class JITFunc(Generic[_P, _T]):
                 self.ir_gen.gen(builder)(**self.tensor_args, **kwargs)
             pf = builder.get()
             pf.orig_func = self.orig_func
-            if builder.out_idx:
-                pf.out_idx_override = builder.out_idx
             return TirTemplate.create(self.orig_func.__name__, pf, builder.constexpr_var, self.ir_gen)
         else:
             raise ValueError(f"Invalid jit mode: {self.mode}, expected 'lazy' or 'eager'")
@@ -1218,9 +1277,8 @@ def prim_func(func: Callable[_P, _T] = None, *, eager_jit: bool = False) -> Prim
                 with builder.prim_func(func.__name__):
                     ir_gen.gen(builder)(**annot)
                 prim_func = builder.get()
+                prim_func = _patch_prim_func_attrs(prim_func, builder)
                 prim_func.orig_func = func
-                if builder.out_idx:
-                    prim_func.out_idx_override = builder.out_idx
                 return prim_func
             except Exception as e:
                 logger.fatal(f"Failed to build prim_func from {func.__name__}\nargs={annot}\nsource={ir_gen.source}")

tile-ai/tilelang-musa

(cherry picked from commit e45ecf7dc4bcb62de96e863991a01e648e5c138c)

testing/python/language/test_tilelang_language_func_attrs.py
@@ -1,0 +1,214 @@
+"""Test T.annotate_compile_flags, T.annotate_pass_configs, and out_idx via PrimFunc attrs."""
+
+import pytest
+import torch
+import tilelang
+from tilelang import language as T
+from tilelang.transform import PassConfigKey
+
+
+def test_out_idx_via_attr_lazy():
+    """out_idx should be stored as PrimFunc attr when using T.empty + return."""
+
+    @T.prim_func
+    def kernel(A):
+        A: T.Tensor[[128, 128], T.float32]
+        B = T.empty([128, 128], T.float32)
+        with T.Kernel(1):
+            for i in T.serial(128):
+                for j in T.serial(128):
+                    B[i, j] = A[i, j] + 1.0
+        return B
+
+    assert "tilelang_out_idx" in kernel.attrs
+    assert list(kernel.attrs["tilelang_out_idx"]) == [-1]
+
+    compiled = tilelang.compile(kernel)
+    a = torch.randn(128, 128, device="musa")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+def test_all_attrs_together_lazy():
+    """annotate_pass_configs, annotate_compile_flags, and out_idx should all work together."""
+
+    @T.prim_func
+    def kernel(A):
+        A: T.Tensor[[64, 64], T.float32]
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        T.annotate_compile_flags(["-ffast-math"])
+        B = T.empty([64, 64], T.float32)
+        with T.Kernel(1):
+            for i in T.serial(64):
+                for j in T.serial(64):
+                    B[i, j] = A[i, j] * 2.0
+        return B
+
+    attrs = kernel.attrs
+    assert "tilelang_out_idx" in attrs
+    assert "tilelang_pass_configs" in attrs
+    assert "tilelang_compile_flags" in attrs
+
+    compiled = tilelang.compile(kernel)
+    a = torch.randn(64, 64, device="musa")
+    b = compiled(a)
+    torch.testing.assert_close(b, a * 2.0)
+
+
+def test_eager_mode_attrs():
+    """Eager mode should support annotate_pass_configs and out_idx via T.empty."""
+
+    @tilelang.jit
+    def kernel(A):
+        M, N = T.const("M N")
+        A: T.Tensor[[M, N], T.float32]
+        B = T.empty([M, N], T.float32)
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        with T.Kernel(1):
+            for i in T.serial(M):
+                for j in T.serial(N):
+                    B[i, j] = A[i, j] + 1.0
+        return B
+
+    a = torch.randn(32, 32, device="musa")
+    result = kernel(a)
+    torch.testing.assert_close(result, a + 1.0)
+
+
+def test_out_idx_conflict_detection():
+    """Specifying both T.empty return and external out_idx should raise ValueError."""
+
+    @T.prim_func
+    def kernel(A):
+        A: T.Tensor[[32, 32], T.float32]
+        B = T.empty([32, 32], T.float32)
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j]
+        return B
+
+    with pytest.raises(ValueError, match="Out index conflict"):
+        tilelang.compile(kernel, out_idx=[-1])
+
+
+def test_no_out_idx_when_not_using_empty():
+    """When T.empty is not used, tilelang_out_idx attr should not be present."""
+
+    @T.prim_func
+    def kernel(A, B):
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j]
+
+    assert kernel.attrs is None or "tilelang_out_idx" not in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="musa")
+    b = compiled(a)
+    torch.testing.assert_close(b, a)
+
+
+def test_pass_configs_only_lazy():
+    """annotate_pass_configs should work without T.empty or annotate_compile_flags."""
+
+    @T.prim_func
+    def kernel(A, B):
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j] + 1.0
+
+    assert "tilelang_pass_configs" in kernel.attrs
+    assert kernel.attrs is None or "tilelang_out_idx" not in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="musa")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+def test_compile_flags_only_lazy():
+    """annotate_compile_flags should work standalone."""
+
+    @T.prim_func
+    def kernel(A, B):
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        T.annotate_compile_flags(["-ffast-math"])
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j] + 1.0
+
+    assert "tilelang_compile_flags" in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="musa")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+def test_annotations_before_tensor_type():
+    """Annotations placed before tensor type annotations should work."""
+
+    @T.prim_func
+    def kernel(A, B):
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        T.annotate_compile_flags(["-ffast-math"])
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j] + 1.0
+
+    assert "tilelang_pass_configs" in kernel.attrs
+    assert "tilelang_compile_flags" in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="musa")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+def test_annotations_after_tensor_type():
+    """Annotations placed after tensor type annotations should work."""
+
+    @T.prim_func
+    def kernel(A, B):
+        A: T.Tensor[[32, 32], T.float32]
+        B: T.Tensor[[32, 32], T.float32]
+        T.annotate_pass_configs({PassConfigKey.TL_ENABLE_FAST_MATH: True})
+        T.annotate_compile_flags(["-ffast-math"])
+        with T.Kernel(1):
+            for i in T.serial(32):
+                for j in T.serial(32):
+                    B[i, j] = A[i, j] + 1.0
+
+    assert "tilelang_pass_configs" in kernel.attrs
+    assert "tilelang_compile_flags" in kernel.attrs
+
+    compiled = tilelang.compile(kernel, out_idx=[-1])
+    a = torch.randn(32, 32, device="musa")
+    b = compiled(a)
+    torch.testing.assert_close(b, a + 1.0)
+
+
+if __name__ == "__main__":
+    test_out_idx_via_attr_lazy()
+    test_all_attrs_together_lazy()
+    test_eager_mode_attrs()
+    test_out_idx_conflict_detection()
+    test_no_out_idx_when_not_using_empty()
+    test_pass_configs_only_lazy()
+    test_compile_flags_only_lazy()
+    test_annotations_before_tensor_type()
+    test_annotations_after_tensor_type()
+    print("All tests passed!")

tilelang/language/eager/builder.py
@@ -181,6 +181,8 @@ class Builder(BaseBuilder):
         self.constexpr_var = set()
         self.eager_jit: EagerJITStage = "none"
         self.eager_jit_subs: dict[str, PrimExpr] = {}
+        self.func_pass_configs: dict[str, Any] | None = None
+        self.func_compile_flags: list[str] | str | None = None
         self.current_file = "<unknown>"
         self.current_line = 0
         self.current_macro_name = "<unknown-macro>"
@@ -761,7 +763,6 @@ if TYPE_CHECKING:
         span: Span | None
         ir_gen: IRGenerator[_P, _T] | None
         orig_func: Callable[_P, _T] | None
-        out_idx_override: list[int] | None
 
 else:
     PrimFunc = tvm.tir.PrimFunc
@@ -942,6 +943,67 @@ def const(name: str, dtype: str = "int32") -> Var | tuple[Var, ...]:
             return builder.eager_jit_subs[name]
 
 
+def annotate_compile_flags(flags: list[str] | str) -> None:
+    """
+    Annotate additional device compile flags inside a function body.
+
+    The flags will be merged with any externally provided compile_flags
+    at compilation time. Can be placed before or after tensor type annotations.
+
+    Example::
+
+        @tilelang.jit
+        def kernel(A, B):
+            T.annotate_compile_flags(["--use_fast_math"])
+            ...
+    """
+    builder = Builder.current()
+    if builder is None:
+        raise JITNoBuilderError("T.annotate_compile_flags() can only be used inside @tilelang.jit or @T.prim_func")
+    if builder.eager_jit == "phase1":
+        return
+    builder.func_compile_flags = flags
+
+
+def annotate_pass_configs(configs: dict[str, Any]) -> None:
+    """
+    Annotate pass configuration inside a function body.
+
+    The configs will be merged with any externally provided pass_configs
+    at compilation time (function-level configs take lower priority, i.e.
+    external configs override). Can be placed before or after tensor type annotations.
+
+    Example::
+
+        @tilelang.jit
+        def kernel(A, B):
+            T.annotate_pass_configs({
+                PassConfigKey.TL_ENABLE_FAST_MATH: True,
+            })
+            ...
+    """
+    builder = Builder.current()
+    if builder is None:
+        raise JITNoBuilderError("T.annotate_pass_configs() can only be used inside @tilelang.jit or @T.prim_func")
+    if builder.eager_jit == "phase1":
+        return
+    builder.func_pass_configs = configs
+
+
+def _patch_prim_func_attrs(pf: PrimFunc, builder: Builder) -> PrimFunc:
+    """Attach function-level out_idx, pass_configs and compile_flags as PrimFunc attrs."""
+    if builder.out_idx:
+        pf = pf.with_attr("tilelang_out_idx", builder.out_idx)
+    if builder.func_pass_configs is not None:
+        pf = pf.with_attr("tilelang_pass_configs", builder.func_pass_configs)
+    if builder.func_compile_flags is not None:
+        flags = builder.func_compile_flags
+        if isinstance(flags, str):
+            flags = [flags]
+        pf = pf.with_attr("tilelang_compile_flags", flags)
+    return pf
+
+
 @dataclass
 class TirTemplate(Generic[_P, _T]):
     """
@@ -1026,8 +1088,7 @@ class TirTemplate(Generic[_P, _T]):
         with builder.prim_func(self.name):
             self.ir_gen.gen(builder)(**tensor_args, **kwargs)
         pf = builder.get()
-        if builder.out_idx:
-            pf.out_idx_override = builder.out_idx
+        pf = _patch_prim_func_attrs(pf, builder)
         return pf
 
 
@@ -1124,8 +1185,6 @@ class JITFunc(Generic[_P, _T]):
                 self.ir_gen.gen(builder)(**self.tensor_args, **kwargs)
             pf = builder.get()
             pf.orig_func = self.orig_func
-            if builder.out_idx:
-                pf.out_idx_override = builder.out_idx
             return TirTemplate.create(self.orig_func.__name__, pf, builder.constexpr_var, self.ir_gen)
         else:
             raise ValueError(f"Invalid jit mode: {self.mode}, expected 'lazy' or 'eager'")
@@ -1225,6 +1284,7 @@ def prim_func(
             with builder.prim_func(func.__name__):
                 ir_gen.gen(builder)(**call_kwargs)
             built = builder.get()
+            built = _patch_prim_func_attrs(built, builder)
             built.orig_func = func
             if builder.out_idx:
                 built.out_idx_override = builder.out_idx
@@ -1246,6 +1306,7 @@ def prim_func(
                 k: sig.parameters[k].default for k in tensor_args if sig.parameters[k].default is not sig.parameters[k].empty
             }
             return JITFunc(func, arg_names, tensor_args, tensor_args_defaults, ir_gen)
+
 
         if generator:
             return prim_func_generator