xwhzz/tilelang

[Bugfix] Fix stage-expanded annotated-layout aliases in LayoutInference (#2031)
* Fix annotated layout inference for dtype-changing views

* lint fix

---------

Co-authored-by: LeiWang1999 <leiwang1999@outlook.com>

testing/python/language/test_tilelang_language_view.py
@@ -124,5 +124,32 @@ def test_view_shared_fp4_to_uint8_compile():
     assert output.dtype == torch.uint8
 
 
+def annotated_layout_on_dtype_changing_view_test():
+    @T.prim_func
+    def main(
+        A: T.Tensor((64, 64), T.float16),
+        B: T.Tensor((64, 128), T.int8),
+    ):
+        with T.Kernel(1, threads=128) as _:
+            A_stage = T.alloc_shared((2, 64, 64), T.float16, scope="shared.dyn")
+            A_i8 = T.view(A_stage, (2, 64, 128), dtype=T.int8)
+            T.annotate_layout({A_i8: T.Layout((2, 64, 128), lambda s, i, j: [s, i, j])})
+
+            for i, j in T.Parallel(64, 64):
+                A_stage[0, i, j] = A[i, j]
+
+            for i, j in T.Parallel(64, 128):
+                B[i, j] = A_i8[0, i, j]
+
+    return main
+
+
+@tilelang.testing.requires_cuda
+def test_annotated_layout_on_dtype_changing_view_compile():
+    program = annotated_layout_on_dtype_changing_view_test()
+    kernel = tl.compile(program, out_idx=-1)
+    assert kernel.get_kernel_source()
+
+
 if __name__ == "__main__":
     tilelang.testing.main()

tile-ai/tilelang-musa

(cherry picked from commit 04468a342549ed112c91a5265ca72517327c2f0e)

testing/python/language/test_tilelang_language_view.py
@@ -154,5 +154,32 @@ def test_view_shared_fp4_to_uint8_compile():
     assert output.dtype == torch.uint8
 
 
+def annotated_layout_on_dtype_changing_view_test():
+    @T.prim_func
+    def main(
+        A: T.Tensor((64, 64), T.float16),
+        B: T.Tensor((64, 128), T.int8),
+    ):
+        with T.Kernel(1, threads=128) as _:
+            A_stage = T.alloc_shared((2, 64, 64), T.float16, scope="shared.dyn")
+            A_i8 = T.view(A_stage, (2, 64, 128), dtype=T.int8)
+            T.annotate_layout({A_i8: T.Layout((2, 64, 128), lambda s, i, j: [s, i, j])})
+
+            for i, j in T.Parallel(64, 64):
+                A_stage[0, i, j] = A[i, j]
+
+            for i, j in T.Parallel(64, 128):
+                B[i, j] = A_i8[0, i, j]
+
+    return main
+
+
+@tilelang.testing.requires_musa
+def test_annotated_layout_on_dtype_changing_view_compile():
+    program = annotated_layout_on_dtype_changing_view_test()
+    kernel = tl.compile(program, out_idx=-1, target="musa")
+    assert kernel.get_kernel_source()
+
+
 if __name__ == "__main__":
     tilelang.testing.main()