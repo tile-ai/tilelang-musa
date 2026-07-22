tile-ai/tilelang

[BugFix] Fix make_int sign-extending negative int8 lanes (#2438)
T.fill of an int8 buffer with a negative scalar (other than -1) silently
wrote only 1 of every 4 elements correctly and clobbered the other 3 to
-1. The vectorized fill packs four int8 lanes into one 32-bit store via
make_int(v,v,v,v); a negative signed char is integer-promoted to int and
sign-extended before the shifts/OR, so its high 1-bits flood the
neighbouring lanes (e.g. make_int(-7,-7,-7,-7) returned 0xFFFFFFF9 =
[-7,-1,-1,-1]).

Build the packed value from explicit unsigned bytes so the sign-extension
bits cannot leak across lanes. Add an int8 negative-fill regression test.

Fixes #2427

testing/python/language/test_tilelang_language_clear.py
@@ -81,5 +81,27 @@ def test_shared_fill_cast_zero_uses_st_bulk():
     assert torch.allclose(out, torch.zeros_like(out))
 
 
+@tilelang.testing.requires_cuda
+def test_fill_int8_negative():
+    M, N = 8, 128
+
+    def program(value):
+        @T.prim_func
+        def main(out: T.Tensor((M, N), "int8")):
+            with T.Kernel(1, threads=128):
+                smem = T.alloc_shared((M, N), "int8")
+                T.fill(smem, value)
+                T.copy(smem, out)
+
+        return main
+
+    for value in (-7, -128, -1, 5):
+        kernel = tilelang.compile(program(value), out_idx=[0])
+        out = kernel()
+        torch.cuda.synchronize()
+        ref = torch.full((M, N), value, dtype=torch.int8, device="cuda")
+        torch.testing.assert_close(out, ref)
+
+
 if __name__ == "__main__":
     test_matmul()

tile-ai/tilelang-musa

(cherry picked from commit 74d4f0ed661280f11c61c89d528d3a8ae360bde5)

testing/python/language/test_tilelang_language_clear.py
@@ -54,5 +54,28 @@ def test_matmul():
     run_matmul(1024, 1024, 1024, 128, 128, 32)
 
 
+def test_fill_int8_negative():
+    import torch
+
+    M, N = 8, 128
+
+    def program(value):
+        @T.prim_func
+        def main(out: T.Tensor((M, N), "int8")):
+            with T.Kernel(1, threads=128):
+                smem = T.alloc_shared((M, N), "int8")
+                T.fill(smem, value)
+                T.copy(smem, out)
+
+        return main
+
+    for value in (-7, -128, -1, 5):
+        kernel = tilelang.compile(program(value), out_idx=[0])
+        out = kernel()
+        torch.musa.synchronize()
+        ref = torch.full((M, N), value, dtype=torch.int8, device="musa")
+        torch.testing.assert_close(out, ref)
+
+
 if __name__ == "__main__":
     test_matmul()